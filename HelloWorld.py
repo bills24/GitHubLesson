f86a9c73af4088629014e6efa37bed3626e8ba45
bills24 <80411428+bills24@users.noreply.github.com>
2021-03-19 14:58:44 +0000

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,4 +2,5 @@
 print("Hello World")
 
 user_name = input("what is your name? ")
-print(f"Hi,{user_name}")
+user_age = input("What is your age?")
+print(f"Hi,{user_name} you are {user_age} years old")
